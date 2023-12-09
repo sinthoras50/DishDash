f8c3827b962f359e5077d4006c88120aba852354
sinthoras50 <sinthoras@hotmail.sk>
2023-12-09 01:13:30 +0100

    Change basename path

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -38,7 +38,7 @@ const DonationManagement = lazy(
 
 const AppRoutes = () => {
   return (
-    <Routes basename={process.env.PUBLIC_URL}>
+    <Routes basename={'https://prod--dishdashproj.netlify.app/'}>
       <Route path="/" element={<Landing />} />
       <PrivateRoute path="admin" element={<Admin />}>
         <PrivateRoute path="/" element={<Home />} />
